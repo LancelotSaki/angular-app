d0b1cad7d895096a648c515665f5c32a4d76794a
Ganhx <1103418874@qq.com>
2018-07-29 01:22:44 +0800

    优化界面

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // 外部引入变量或者常量值，类似于java中的实体类
 import { Profile, Info } from './resume';
+import {formatNumber} from '@angular/common';
 
 @Component({
   selector: 'app-resume',
@@ -9,17 +10,15 @@ import { Profile, Info } from './resume';
 })
 export class ResumeComponent implements OnInit {
   // 1.直接定义直接赋值(赋值使用要用等于号)
-  title = '个人简历';
+  dialog = '';
+  dialogState = 'dialogInit';
   // 2.先定义再使用(定义用冒号)
   angularVersion: string;
   // 3.外部引入再赋值
-  dialog = '';
-  dialogState = false;
   profile: Profile = {
     id: 1,
     name: 'carney'
   };
-  currentStyle = 'head';
   info = [
     new Info('1', 'carney'),
     new Info('2', '180000000'),
@@ -39,35 +38,74 @@ export class ResumeComponent implements OnInit {
     {id: '9', name: 'Linux', value: 82, image: '../../assets/images/skill-icon/linux.svg'}];
   constructor() {
     this.angularVersion = `Angular v6.0.9`;
-    const d = new Date();
-    if (d.getHours() <= 10) {
-      this.currentStyle = 'morningHead';
-    } else if (d.getHours() >= 18) {
-      this.currentStyle = 'nightHead';
-    }
   }
 
   ngOnInit() {
-    const words = '感谢你查看我的简历!';
+    const date = new Date();
+    let words = ``;
+    console.log(date.getHours());
+    const currentTime = date.getHours();
+    if (currentTime > 5 && currentTime <= 8) {
+      words = '早上好!';
+    } else if (currentTime > 8 && currentTime <= 10) {
+      words = '上午好!';
+    } else if (currentTime > 10 && currentTime <= 12) {
+      words = '中午好!';
+    } else if (currentTime > 12 && currentTime <= 18) {
+      words = '下午好!';
+    } else if (currentTime > 18 && currentTime <= 23) {
+      words = '晚上好!';
+    } else if (currentTime >= 0 && currentTime <= 5) {
+      words = '已经深夜了,看什么看!';
+    } else {
+      words = '你好!';
+    }
     // 3s后执行
     window.setTimeout(() => {
-      this.exeDialog(words);
+      this.exeDialog(words).then( () => {
+        this.waitTimeCloseDialog();
+      });
     }, 3000);
-    this.dialogState = true;
+    const nextB = '感谢你查看我的简历!';
+    // 23s后执行
+    window.setTimeout(() => {
+      this.exeDialog(nextB).then( () => {
+        this.waitTimeCloseDialog();
+      });
+    }, 23000);
+    const nextC = '今天吃点什么好呢?';
+    // 40s后执行
+    window.setTimeout(() => {
+      this.exeDialog(nextC).then( () => {
+        this.waitTimeCloseDialog();
+      });
+    }, 40000);
+    const nextD = '今天天气不错?';
+    // 60S后执行
+    window.setTimeout(() => {
+      this.exeDialog(nextD).then( () => {
+        this.waitTimeCloseDialog();
+      });
+    }, 60000);
+    const nextE = '无聊啊,敲代码吧!';
+    // 120s后执行
+    window.setTimeout(() => {
+      this.exeDialog(nextE).then( () => {
+        this.waitTimeCloseDialog();
+      });
+    }, 120000);
     console.log(`开始执行`);
   }
 
   /*同步执行*/
   async exeDialog(words) {
     console.log(`开始内部同步执行`);
+    // await修饰的方法里面返回的必须是promise
+    this.dialogState = 'dialogShow';
     const result = await this.returnSay(words);
     console.log(result);
     console.log(`同步执行结束`);
-    window.setTimeout( () => {
-      this.dialogState = false;
-    }, 3000);
   }
-
   returnSay(words) {
     let x = 0;
     return new Promise((res) => {
@@ -80,5 +118,12 @@ export class ResumeComponent implements OnInit {
         }
       }, 120 );
     });
+  }
+
+  waitTimeCloseDialog() {
+    window.setTimeout( () => {
+      this.dialogState = 'dialogHide';
+      this.dialog = '';
+    }, 3000);
   }
 }
